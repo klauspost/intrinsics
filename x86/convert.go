0ccbfbc34b26ce07dbb399b682a12a50e3fe7701
klauspost <klauspost@gmail.com>
2015-10-28 13:23:35 +0100

    Use unsafe for slice conversion.

--- x86/convert.go
+++ x86/convert.go
@@ -1,56 +1,97 @@
 package x86
 
+import (
+    "reflect"
+    "unsafe"
+)
+
 // FloatToM128 converts a slice into M128 array.
 // The number of elements is len(src) / 4.
-// FIXME: Should simply be a pointer to the original data, not a copy
 func FloatToM128(src []float32) []M128 {
-	l := len(src) / 4
-	dst := make([]M128, l)
-	for i := 0; i < l; i++ {
-		dst[i] = M128{src[i*4], src[i*4+1], src[i*4+2], src[i*4+3]}
-	}
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&src))
+
+	// The length and capacity of the slice are different.
+	header.Len /= 4
+	header.Cap /= 4
+
+	// Convert slice header to an []int32
+	dst := *(*[]M128)(unsafe.Pointer(&header))
+
+	return dst
+}
+
+// M128ToFloat converts M128 slice to a float32 slice.
+// The number of elements is len(src) * 4.
+func M128ToFloat(src []M128) []float32 {
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&src))
+
+	// The length and capacity of the slice are different.
+	header.Len *= 4
+	header.Cap *= 4
+
+	// Convert slice header to an []int32
+	dst := *(*[]float32)(unsafe.Pointer(&header))
+
 	return dst
 }
 
 // BytesToM128i converts a byte slice into M128i array.
 // The number of elements is len(src) / 16.
-// FIXME: Should simply be a pointer to the original data, not a copy
 func BytesToM128i(src []byte) []M128i {
-	l := len(src) / 16
-	dst := make([]M128i, l)
-	for i := 0; i < l; i++ {
-		d := M128i{}
-		for j, n := range src[:16] {
-			d[j] = n
-		}
-		dst[i] = d
-		src = src[16:]
-	}
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&src))
+
+	// The length and capacity of the slice are different.
+	header.Len /= 16
+	header.Cap /= 16
+
+	// Convert slice header to an []int32
+	dst := *(*[]M128i)(unsafe.Pointer(&header))
+
 	return dst
 }
 
 // M128iToBytes converts M128i slice to a byte slice.
 // The number of elements is len(src) * 16.
-// FIXME: Should simply be a pointer to the original data, not a copy
 func M128iToBytes(src []M128i) []byte {
-	l := len(src) * 16
-	dst := make([]byte, l)
-	for i := 0; i < l; i++ {
-		for j, n := range src[i] {
-			dst[i*16+j] = n
-		}
-	}
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&src))
+
+	// The length and capacity of the slice are different.
+	header.Len *= 16
+	header.Cap *= 16
+
+	// Convert slice header to an []int32
+	dst := *(*[]byte)(unsafe.Pointer(&header))
+
 	return dst
 }
 
+
 // DoubleToM128d converts a slice into M128d array.
 // The number of elements is len(src) / 2.
-// FIXME: Should simply be a pointer to the original data, not a copy
 func DoubleToM128d(src []float64) []M128d {
-	l := len(src) / 2
-	dst := make([]M128d, l)
-	for i := 0; i < l; i++ {
-		dst[i] = M128d{src[i*2], src[i*2+1]}
-	}
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&src))
+
+	// The length and capacity of the slice are different.
+	header.Len /= 2
+	header.Cap /= 2
+
+	// Convert slice header to an []int32
+	dst := *(*[]M128d)(unsafe.Pointer(&header))
+
+	return dst
+}
+
+// M128ToFloat converts M128 slice to a float64 slice.
+// The number of elements is len(src) * 2.
+func M128dToDouble(src []M128) []float64 {
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&src))
+
+	// The length and capacity of the slice are different.
+	header.Len *= 2
+	header.Cap *= 2
+
+	// Convert slice header to an []int32
+	dst := *(*[]float64)(unsafe.Pointer(&header))
+
 	return dst
 }
